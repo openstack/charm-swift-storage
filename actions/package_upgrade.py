3a5b5872c5418317b58dbf2299020c75828330a7
Corey Bryant <corey.bryant@canonical.com>
2022-01-07 15:52:09 +0000

    Add package-upgrade action
    The package-upgrade action performs package upgrades for the current
    OpenStack release.
    
    The code path used is similar to the openstack-upgrade action, with the
    difference being that package-upgrade will not execute if an openstack
    upgrade is available (based on the openstack-origin setting).
    
    This change includes a charm-helpers sync.
    
    Change-Id: I0c7184bba29731354e52dc28e3a4dd6f282fa843

---
+++ actions/package_upgrade.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+#
+# Copyright 2022 Canonical Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+
+_path = os.path.dirname(os.path.realpath(__file__))
+_root = os.path.abspath(os.path.join(_path, '..'))
+
+
+def _add_path(path):
+    if path not in sys.path:
+        sys.path.insert(1, path)
+
+
+_add_path(_root)
+
+
+from charmhelpers.contrib.openstack.utils import (
+    do_action_package_upgrade,
+)
+
+from hooks.swift_storage_hooks import (
+    config_changed,
+    CONFIGS,
+)
+
+from lib.swift_storage_utils import (
+    do_openstack_upgrade,
+)
+
+
+def package_upgrade():
+    """Perform package upgrade within the current OpenStack release.
+
+    In order to prevent this action from upgrading to a new release of
+    OpenStack, package upgrades are not run if a new OpenStack release is
+    available. See source of do_action_package_upgrade() for this check.
+
+    Upgrades packages and sets the corresponding action status as a result."""
+
+    if (do_action_package_upgrade('swift',
+                                  do_openstack_upgrade,
+                                  CONFIGS)):
+        config_changed()
+
+
+if __name__ == '__main__':
+    package_upgrade()
